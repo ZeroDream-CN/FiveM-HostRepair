328f83e4781cac77d70e5dd9087afee3f749b895
kasuganosoras <kasuganosora@tcotp.cn>
2020-12-23 01:28:13 +0800

    Change to get hosts from server and add let's encrypt fix

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/eddieivan01/nic"
+	"github.com/json-iterator/go"
 	"log"
 	"io/ioutil"
 	"strings"
@@ -11,6 +12,8 @@ import (
 )
 
 var HttpClient nic.Session
+var hostResult RequestResult
+var hostPath   string
 
 func main() {
 	
@@ -18,97 +21,118 @@ func main() {
 	log.SetFlags(log.Ltime)
 	log.Println("FiveM Host 修复工具 by Akkariin")
 	
-	// 获取服务器 IP
-	log.Println("正在获取最新的 Host...")
-	response, err := HttpClient.Get("https://api.zerodream.net/v1/fivemclient/host", nil)
-	if err != nil {
-		// 获取失败
-		log.Println("无法获取服务器 IP，请稍后重试，如问题持续存在请联系 QQ 204034。")
-		log.Fatalln(fmt.Sprintf("错误内容：%s", err.Error()))
-	}
-	log.Println(fmt.Sprintf("获得服务器 IP：%s", response.Text))
-	
 	// 设定 Hosts 路径
-	hostPath := ""
 	if runtime.GOOS == "windows" {
 		hostPath = "C:/Windows/System32/drivers/etc/hosts"
 	} else {
 		hostPath = "/etc/hosts"
 	}
 	
-	// 解析 Hosts 文件
-	hostFile := file_get_contents(hostPath)
-	allLines := strings.Split(hostFile, "\n")
-	newHosts := ""
-	serverIp := response.Text
-	lambdaEx := false
-	mirrorEx := false
-	policyEx := false
-	serverEx := false
-	svliveEx := false
-	
-	// 循环读取每一行内容
-	for _, line := range allLines {
-		if strings.Contains(line, "lambda.fivem.net") {
-			log.Println("找到 lambda.fivem.net 的记录，已更新")
-			newHosts += fmt.Sprintf("%s %s\n", serverIp, "lambda.fivem.net")
-			lambdaEx = true
-		} else if strings.Contains(line, "mirrors.fivem.net") {
-			log.Println("找到 mirrors.fivem.net 的记录，已更新")
-			newHosts += fmt.Sprintf("%s %s\n", serverIp, "mirrors.fivem.net")
-			mirrorEx = true
-		} else if strings.Contains(line, "policy-is.fivem.net") {
-			log.Println("找到 policy-is.fivem.net 的记录，已更新")
-			newHosts += fmt.Sprintf("%s %s\n", serverIp, "policy-is.fivem.net")
-			policyEx = true
-		} else if strings.Contains(line, "servers-frontend.fivem.net") {
-			log.Println("找到 servers-frontend.fivem.net 的记录，已更新")
-			newHosts += fmt.Sprintf("%s %s\n", serverIp, "servers-frontend.fivem.net")
-			serverEx = true
-		} else if strings.Contains(line, "servers-live.fivem.net") {
-			log.Println("找到 servers-live.fivem.net 的记录，已更新")
-			newHosts += fmt.Sprintf("%s %s\n", serverIp, "servers-live.fivem.net")
-			svliveEx = true
+	log.Println("正在检测网络状态...")
+	_, err := HttpClient.Get("http://ocsp.int-x3.letsencrypt.org/", nic.H {
+		Timeout: 5,
+	})
+
+	if err != nil {
+		if strings.Contains(err.Error(), "Timeout") {
+			log.Println("检测到 Let's Encrypt 服务器可能被 DNS 污染，修复中...")
+			if FixLetsEncrypt() {
+				log.Println("Let's Encrypt 服务器 Hosts 修复成功")
+			} else {
+				log.Println("Let's Encrypt 服务器 Hosts 修复失败！")
+				if runtime.GOOS == "windows" {
+					log.Fatalln("请检查是否用 [右键 -> 以管理员身份运行] 本程序，如果使用管理员权限运行后还是出错，请手动打开 C:/Windows/System32/drivers/etc/ 并右键查看 hosts 文件是否被设置为了只读，如果是请将只读取消然后重试。如果问题持续存在，请联系 QQ 204034。")
+				} else {
+					log.Fatalln("请检查 /etc/hosts 文件是否是只读状态，以及您当前的用户组是否有读写权限，请尝试使用 root 身份执行本操作！")
+				}
+			}
 		} else {
+			log.Println("检测时发生了未知错误")
+			log.Fatalln(err.Error())
+		}
+	}
+	
+	// 获取服务器 IP
+	log.Println("正在获取最新的 Host...")
+	response, err := HttpClient.Get("https://api.zerodream.net/v2/fivemclient/host", nil)
+	if err != nil {
+		// 获取失败
+		log.Println("无法获取服务器 IP，请稍后重试，如问题持续存在请联系 QQ 204034。")
+		log.Fatalln(fmt.Sprintf("错误内容：%s", err.Error()))
+	}
+	
+	err = jsoniter.Unmarshal(response.Bytes, &hostResult)
+	
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("无法解析服务器返回的数据，请稍后重试。服务器返回内容：%s", response.Text))
+	}
+	
+	log.Println(fmt.Sprintf("获得服务器 IP：%s", hostResult.Ip))
+	
+	// 解析 Hosts 文件
+	hostFile := GetFileData(hostPath)
+	
+	for _, hostName := range hostResult.Host {
+		log.Println(fmt.Sprintf("已更新 %s 的 IP 地址为 %s", hostName, hostResult.Ip))
+		hostFile = SetHosts(hostFile, hostName, hostResult.Ip)
+	}
+	
+	// 补齐结尾换行
+	hostFile += "\n"
+	
+	// 写入到文件中
+	if SetFileData(hostPath, hostFile) {
+		log.Println("Hosts 文件写入成功！")
+	} else {
+		log.Println("Hosts 文件写入失败！")
+		if runtime.GOOS == "windows" {
+			log.Fatalln("请检查是否用 [右键 -> 以管理员身份运行] 本程序，如果使用管理员权限运行后还是出错，请手动打开 C:/Windows/System32/drivers/etc/ 并右键查看 hosts 文件是否被设置为了只读，如果是请将只读取消然后重试。如果问题持续存在，请联系 QQ 204034。")
+		} else {
+			log.Fatalln("请检查 /etc/hosts 文件是否是只读状态，以及您当前的用户组是否有读写权限，请尝试使用 root 身份执行本操作！")
+		}
+	}
+	
+	fmt.Printf("\n请按回车键退出...")
+    os.Stdin.Read(make([]byte, 1))
+}
+
+// 修复 Let's Encrypt 服务器被劫持问题
+func FixLetsEncrypt() (bool) {
+	
+	// 读取 Hosts 文件
+	hostFile := GetFileData(hostPath)
+	hostFile  = SetHosts(hostFile, "ocsp.int-x3.letsencrypt.org", "23.46.210.176")
+	
+	return SetFileData(hostPath, hostFile)
+}
+
+// 设置 Hosts
+func SetHosts(hostData string, hostName string, ipAddr string) (string) {
+	
+	// 是否找到 Hosts
+	foundHost := false
+	
+	allLines := strings.Split(hostData, "\n")
+	newHosts := ""
+	
+	for _, line := range allLines {
+		if strings.Contains(line, fmt.Sprintf(" %s", hostName)) {
+			newHosts += fmt.Sprintf("%s %s\n", ipAddr, hostName)
+			foundHost = true
+		} else if line != "" {
 			newHosts += line + "\n"
 		}
 	}
 	
-	// 判断是否有不存在的记录
-	if !lambdaEx {
-		log.Println("未找到 lambda.fivem.net 的记录，已添加")
-		newHosts += fmt.Sprintf("%s %s\n", serverIp, "lambda.fivem.net")
-	}
-	if !mirrorEx {
-		log.Println("未找到 mirrors.fivem.net 的记录，已添加")
-		newHosts += fmt.Sprintf("%s %s\n", serverIp, "mirrors.fivem.net")
-	}
-	if !policyEx {
-		log.Println("未找到 policy-is.fivem.net 的记录，已添加")
-		newHosts += fmt.Sprintf("%s %s\n", serverIp, "policy-is.fivem.net")
-	}
-	if !serverEx {
-		log.Println("未找到 servers-frontend.fivem.net 的记录，已添加")
-		newHosts += fmt.Sprintf("%s %s\n", serverIp, "servers-frontend.fivem.net")
-	}
-	if !svliveEx {
-		log.Println("未找到 servers-live.fivem.net 的记录，已添加")
-		newHosts += fmt.Sprintf("%s %s\n", serverIp, "servers-live.fivem.net")
+	if !foundHost {
+		newHosts += fmt.Sprintf("%s %s\n", ipAddr, hostName)
 	}
 	
-	// 写入到文件中
-	if file_put_contents(hostPath, newHosts) {
-		log.Println("Hosts 文件写入成功！")
-	} else {
-		log.Println("Hosts 文件写入失败，可能是因为没有权限，请尝试右键使用管理员身份运行！（Linux 系统下使用 sudo 命令）")
-	}
-	
-	fmt.Printf("\n请按任意键退出...")
-    os.Stdin.Read(make([]byte, 1))
+	return newHosts
 }
 
 // 读文件
-func file_get_contents(fileName string) (string) {
+func GetFileData(fileName string) (string) {
     f, err := ioutil.ReadFile(fileName)
     if err != nil {
         log.Fatalln("Failed to read file", err)
@@ -117,11 +141,17 @@ func file_get_contents(fileName string) (string) {
 }
 
 // 写文件
-func file_put_contents(fileName string, data string) (bool) {
+func SetFileData(fileName string, data string) (bool) {
 	var bytes = []byte(data)
 	err := ioutil.WriteFile(fileName, bytes, 0666)
 	if err != nil {
 		return false
 	}
 	return true
+}
+
+// 定义服务器返回文件类型
+type RequestResult struct {
+	Ip   string
+	Host []string
 }
